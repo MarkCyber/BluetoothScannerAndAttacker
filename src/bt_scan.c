4963163ff425eae32464ce475db834eb85c8e37c
Mark <148797232+MarkCyber@users.noreply.github.com>
2024-06-10 10:53:54 -0400

    Update bt_scan.c

--- src/bt_scan.c
+++ src/bt_scan.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
@@ -12,26 +13,33 @@ int main(int argc, char **argv) {
     char name[248] = { 0 };
 
     dev_id = hci_get_route(NULL);
-    sock = hci_open_dev(dev_id);
-    if (dev_id < 0 || sock < 0) {
-        perror("opening socket");
+    if (dev_id < 0) {
+        perror("No Bluetooth Adapter Available");
         exit(1);
     }
 
-    len = 8;
+    sock = hci_open_dev(dev_id);
+    if (sock < 0) {
+        perror("Opening socket");
+        exit(1);
+    }
+
+    len = 8;  // Inquiry duration * 1.28s
     max_rsp = 255;
     flags = IREQ_CACHE_FLUSH;
     ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
 
     num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
-    if (num_rsp < 0) perror("hci_inquiry");
-
-    for (int i = 0; i < num_rsp; i++) {
-        ba2str(&(ii+i)->bdaddr, addr);
-        memset(name, 0, sizeof(name));
-        if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name), name, 0) < 0)
-            strcpy(name, "[unknown]");
-        printf("%s  %s\n", addr, name);
+    if (num_rsp < 0) {
+        perror("hci_inquiry");
+    } else {
+        for (int i = 0; i < num_rsp; i++) {
+            ba2str(&(ii[i].bdaddr), addr);
+            memset(name, 0, sizeof(name));
+            if (hci_read_remote_name(sock, &(ii[i].bdaddr), sizeof(name), name, 0) < 0)
+                strcpy(name, "[unknown]");
+            printf("%s  %s\n", addr, name);
+        }
     }
 
     free(ii);
